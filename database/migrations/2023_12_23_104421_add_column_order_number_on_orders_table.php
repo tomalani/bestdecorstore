115ef6fb22e4e71370db3bac73a917390ad8c51a
Neonartit <neon@buffalomarket.com>
2023-12-24 15:21:10 +0700

    add_column order_number

--- database/migrations/2023_12_23_104421_add_column_order_number_on_orders_table.php
+++ database/migrations/2023_12_23_104421_add_column_order_number_on_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->string('order_number')->after('id'); // Adjust the position as needed
+            $table->string('order_number')->after('id')->nullable(); // Adjust the position as needed
         });
     }
 
