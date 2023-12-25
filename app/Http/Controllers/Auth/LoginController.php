f52b3a316fb7d4cf3af59f76d0b541729af9e92c
Neonartit <neon@buffalomarket.com>
2023-12-25 21:44:25 +0700

    middleware logout login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = route('backend-products');
+    // protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -36,5 +37,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function redirectTo()
+    {
+        return route('backend-products');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use app\Http\Controllers\ProductsController;
 use app\Http\Controllers\CategoriesController;
 use app\Http\Controllers\UsersController;
+use app\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Auth;
 
 /*
@@ -24,42 +25,42 @@ Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('ho
 
 
 Auth::routes();
+Route::middleware(['auth.check'])->group(function () {
+    // backend products
+    Route::get('/backend/products', [App\Http\Controllers\ProductsController::class, 'index'])->name('backend-products');
+    Route::get('/backend/products/add', [App\Http\Controllers\ProductsController::class, 'add']);
+    Route::post('/backend/products/insert', [App\Http\Controllers\ProductsController::class, 'insert']);
+    Route::get('/backend/products/edit/{id}', [App\Http\Controllers\ProductsController::class, 'edit']);
+    Route::post('/backend/products/update', [App\Http\Controllers\ProductsController::class, 'update']);
 
-// backend products
-Route::get('/backend/products', [App\Http\Controllers\ProductsController::class, 'index'])->name('backend-products');
-Route::get('/backend/products/add', [App\Http\Controllers\ProductsController::class, 'add']);
-Route::post('/backend/products/insert', [App\Http\Controllers\ProductsController::class, 'insert']);
-Route::get('/backend/products/edit/{id}', [App\Http\Controllers\ProductsController::class, 'edit']);
-Route::post('/backend/products/update', [App\Http\Controllers\ProductsController::class, 'update']);
+    // backend categories
+    Route::get('/backend/categories', [App\Http\Controllers\CategoriesController::class, 'index'])->name('backend-categories');;
+    Route::get('/backend/categories/add', [App\Http\Controllers\CategoriesController::class, 'add']);
+    Route::post('/backend/categories/insert', [App\Http\Controllers\CategoriesController::class, 'insert']);
+    Route::get('/backend/categories/edit/{id}', [App\Http\Controllers\CategoriesController::class, 'edit']);
+    Route::post('/backend/categories/update', [App\Http\Controllers\CategoriesController::class, 'update']);
 
-// backend categories
-Route::get('/backend/categories', [App\Http\Controllers\CategoriesController::class, 'index'])->name('backend-categories');
-;
-Route::get('/backend/categories/add', [App\Http\Controllers\CategoriesController::class, 'add']);
-Route::post('/backend/categories/insert', [App\Http\Controllers\CategoriesController::class, 'insert']);
-Route::get('/backend/categories/edit/{id}', [App\Http\Controllers\CategoriesController::class, 'edit']);
-Route::post('/backend/categories/update', [App\Http\Controllers\CategoriesController::class, 'update']);
+    // backend user 
+    Route::get('/backend/users', [App\Http\Controllers\UsersController::class, 'index'])->name('backend-users');;
+    Route::get('/backend/users/add', [App\Http\Controllers\CategoriesController::class, 'add']);
+    Route::post('/backend/users/insert', [App\Http\Controllers\CategoriesController::class, 'insert']);
+    Route::get('/backend/users/edit/{id}', [App\Http\Controllers\CategoriesController::class, 'edit']);
+    Route::get('/backend/users/delete/{id}', [App\Http\Controllers\CategoriesController::class, 'delete']);
+    Route::post('/backend/users/update', [App\Http\Controllers\CategoriesController::class, 'update']);
 
-// backend user 
-Route::get('/backend/users', [App\Http\Controllers\UsersController::class, 'index'])->name('backend-users');;
-Route::get('/backend/users/add', [App\Http\Controllers\CategoriesController::class, 'add']);
-Route::post('/backend/users/insert', [App\Http\Controllers\CategoriesController::class, 'insert']);
-Route::get('/backend/users/edit/{id}', [App\Http\Controllers\CategoriesController::class, 'edit']);
-Route::get('/backend/users/delete/{id}', [App\Http\Controllers\CategoriesController::class, 'delete']);
-Route::post('/backend/users/update', [App\Http\Controllers\CategoriesController::class, 'update']);
+    // backend order  
+    Route::get('/backend/orders', [App\Http\Controllers\OrderController::class, 'index'])->name('backend-orders');
+    Route::get('/backend/orders/add', [App\Http\Controllers\OrderController::class, 'add']);
+    Route::post('/backend/orders/insert', [App\Http\Controllers\OrderController::class, 'insert']);
+    Route::get('/backend/orders/edit/{id}', [App\Http\Controllers\OrderController::class, 'edit']);
+    Route::get('/backend/orders/delete/{id}', [App\Http\Controllers\OrderController::class, 'delete']);
+    Route::post('/backend/orders/update', [App\Http\Controllers\OrderController::class, 'update']);
 
-// backend order  
-Route::get('/backend/orders', [App\Http\Controllers\OrderController::class, 'index'])->name('backend-orders');
-Route::get('/backend/orders/add', [App\Http\Controllers\OrderController::class, 'add']);
-Route::post('/backend/orders/insert', [App\Http\Controllers\OrderController::class, 'insert']);
-Route::get('/backend/orders/edit/{id}', [App\Http\Controllers\OrderController::class, 'edit']);
-Route::get('/backend/orders/delete/{id}', [App\Http\Controllers\OrderController::class, 'delete']);
-Route::post('/backend/orders/update', [App\Http\Controllers\OrderController::class, 'update']);
+    Route::post('/backend/orders/confirm-order', [App\Http\Controllers\OrderController::class, 'confirm_order']);
+    Route::post('/backend/orders/shipped-order', [App\Http\Controllers\OrderController::class, 'shipped_order']);
+    Route::post('/backend/orders/delivery-order', [App\Http\Controllers\OrderController::class, 'delivery_order']);
+    Route::get('/backend/orders/reject-order/{id}', [App\Http\Controllers\OrderController::class, 'reject_order']);
 
-Route::post('/backend/orders/confirm-order', [App\Http\Controllers\OrderController::class, 'confirm_order']);
-Route::post('/backend/orders/shipped-order', [App\Http\Controllers\OrderController::class, 'shipped_order']);
-Route::post('/backend/orders/delivery-order', [App\Http\Controllers\OrderController::class, 'delivery_order']);
-Route::get('/backend/orders/reject-order/{id}', [App\Http\Controllers\OrderController::class, 'reject_order']);
-
-//ajax backend order
-Route::get('/backend/ajax-orders-item', [App\Http\Controllers\OrderController::class, 'ajax_order_items'])->name('backend.orders.item.ajax');
+    //ajax backend order
+    Route::get('/backend/ajax-orders-item', [App\Http\Controllers\OrderController::class, 'ajax_order_items'])->name('backend.orders.item.ajax');
+});
